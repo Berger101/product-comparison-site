22e431a259bc060ec4e2ec2a1e3e2d17f91e7386
Erik Berger <berger.erik12@gmail.com>
2024-09-19 17:41:30 +0200

    Added baseURL to axiosdefaults

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,3 +1,14 @@
+import axios from "axios";
+
+// Pointing to the Django backend running on port 8000
+axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
+
+ 
  // IMPORTANT!!
  // Because this React app is running in the same workspace as the API,
 
