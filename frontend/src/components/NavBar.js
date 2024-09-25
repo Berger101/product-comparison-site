d943ae2602247584085d8519613a7462b8992485
Erik Berger <berger.erik12@gmail.com>
2024-09-25 12:46:19 +0200

    NavBar burger toggle with a custom hook

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -9,11 +9,14 @@ import {
 } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
   const location = useLocation();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const getNavLinkClass = (path) =>
     location.pathname === path
@@ -78,7 +81,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -86,7 +89,7 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink className={getNavLinkClass("/")} to="/">
