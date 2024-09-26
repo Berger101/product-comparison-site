6081c644c7c9d148faeaa109c900421f44ee62a5
Erik Berger <berger.erik12@gmail.com>
2024-09-26 15:51:43 +0200

    Changed avatar so it defines the correct URL path, will be changed later to hide URL path

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "../styles/Avatar.module.css";
 
+// Define the fallback default image URL
+const defaultAvatar = "https://res.cloudinary.com/dmmoveeu5/image/upload/v1723554985/default_profile_vakdxh.jpg";
+
 const Avatar = ({ src, height = 45, text }) => {
+  // Use the provided src, or fallback to the default image if src is not provided
+  const imageSrc = src || defaultAvatar;
+
   return (
     <span>
       <img
         className={styles.Avatar}
-        src={src}
+        src={imageSrc}
         height={height}
         width={height}
         alt="avatar"
