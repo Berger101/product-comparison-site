501d0c18518cdb8af676e1123d121344fdfd5a73
Erik Berger <berger.erik12@gmail.com>
2024-10-03 16:13:15 +0200

    Added counts for posts, followers and following to serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Profile
 from followers.models import Follower
+from posts.models import Post
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -8,6 +9,11 @@ class ProfileSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
     profile_image = serializers.ReadOnlyField(source='image.url')
+
+    # count posts, followers, and following
+    posts_count = serializers.SerializerMethodField()
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -19,13 +25,25 @@ class ProfileSerializer(serializers.ModelSerializer):
             following = Follower.objects.filter(
                 owner=user, followed=obj.owner
             ).first()
-            # print(following)
             return following.id if following else None
         return None
+
+    def get_posts_count(self, obj):
+        # Count the number of posts the profile owner has created
+        return Post.objects.filter(owner=obj.owner).count()
+
+    def get_followers_count(self, obj):
+        # Count how many users are following this profile's owner
+        return Follower.objects.filter(followed=obj.owner).count()
+
+    def get_following_count(self, obj):
+        # Count how many users this profile's owner is following
+        return Follower.objects.filter(owner=obj.owner).count()
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner', 'following_id', 'profile_image'
+            'content', 'image', 'is_owner', 'following_id', 'profile_image',
+            'posts_count', 'followers_count', 'following_count'
         ]
