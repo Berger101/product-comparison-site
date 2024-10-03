f09ee0b0bd23483482db3bf18e90be5bb98d0b58
Erik Berger <berger.erik12@gmail.com>
2024-10-03 12:45:08 +0200

    Updated image models for profiles

--- profiles/models.py
+++ profiles/models.py
@@ -10,8 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='default_profile_vakdxh',
-        blank=True
+        upload_to='images/', default='../default_post_zblkfc', blank=True, null=True
     )
 
     class Meta:
