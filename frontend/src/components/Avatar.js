2303671c78590fbd3611de1d292df4abb91f4cc6
Erik Berger <berger.erik12@gmail.com>
2024-10-03 15:15:33 +0200

    Configuring avatars src url path

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/Avatar.module.css";
 
 // Define the fallback default image URL
-const defaultAvatar = "https://res.cloudinary.com/dmmoveeu5/image/upload/v1723554985/default_profile_vakdxh.jpg";
+const defaultAvatar = "https://res.cloudinary.com/dmmoveeu5/image/upload/v1/default_profile_vakdxh.jpg";
 
 const Avatar = ({ src, height = 45, text }) => {
   // Use the provided src, or fallback to the default image if src is not provided
