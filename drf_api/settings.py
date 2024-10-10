fb06213495e88cca799ddbc567daebd9aa14cbce
Erik Berger <berger.erik12@gmail.com>
2024-10-10 15:28:23 +0200

    Setting debug and dev to False

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -57,7 +57,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+# DEBUG = 'DEBUG' in os.environ
 
 # Get ALLOWED_HOSTS from environment variables, with 'localhost' as a fallback
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
