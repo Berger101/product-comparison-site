9a82fd068165f294720335285dcb5944bf673445
Erik Berger <berger.erik12@gmail.com>
2024-10-10 14:22:47 +0200

    Telling Django and WhiteNoise where to look for Reacts index.html file in deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -133,7 +133,7 @@ ROOT_URLCONF = 'drf_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'staticfiles', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
