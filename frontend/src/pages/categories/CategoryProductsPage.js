e046305d8833ed6d1fad459adb4c5a84287f92e9
Erik Berger <berger.erik12@gmail.com>
2024-10-21 15:32:08 +0200

    Updated category detail page so each product works properly within the page

--- frontend/src/pages/categories/CategoryProductsPage.js
+++ frontend/src/pages/categories/CategoryProductsPage.js
@@ -1,39 +1,63 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Product from "../products/Product";
 import styles from "../../styles/CategoryProductsPage.module.css";
+import { Container } from "react-bootstrap";
+import Asset from "../../components/Asset";
+import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function CategoryProductsPage() {
   const { category } = useParams();
-  const [products, setProducts] = useState([]);
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [categoryProducts, setCategoryProducts] = useState({ results: [] });
 
   useEffect(() => {
-    const fetchProducts = async () => {
+    const fetchData = async () => {
       try {
         const { data } = await axiosReq.get(`/products/?category=${category}`);
-        setProducts(data.results);
+        setCategoryProducts(data);
+        setHasLoaded(true);
       } catch (err) {
         console.log(err);
       }
     };
 
-    fetchProducts();
+    fetchData();
   }, [category]);
 
+  const categoryTitle = category.charAt(0).toUpperCase() + category.slice(1);
+
   return (
-    <div className={styles.CategoryProductsPage}>
-      <h2>{category.charAt(0).toUpperCase() + category.slice(1)}</h2>
-      <div className={styles.ProductList}>
-        {products.length > 0 ? (
-          products.map((product) => (
-            <Product key={product.id} {...product} setProducts={setProducts} />
-          ))
+    <Container className={styles.CategoryProductsPage}>
+      <h2 className="text-center">{categoryTitle}</h2>
+      {hasLoaded ? (
+        categoryProducts.results.length ? (
+          <InfiniteScroll
+            children={categoryProducts.results.map((product) => (
+              <Product
+                key={product.id}
+                {...product}
+                setProducts={setCategoryProducts}
+              />
+            ))}
+            dataLength={categoryProducts.results.length}
+            loader={<Asset spinner />}
+            hasMore={!!categoryProducts.next}
+            next={() => fetchMoreData(categoryProducts, setCategoryProducts)}
+          />
         ) : (
-          <p>No products found for this category.</p>
-        )}
-      </div>
-    </div>
+          <Asset
+            src={NoResults}
+            message={`No products found for the category "${categoryTitle}".`}
+          />
+        )
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
   );
 }
 
