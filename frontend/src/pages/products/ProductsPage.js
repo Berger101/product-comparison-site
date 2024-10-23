3f671ae047191e56a8390b721e1f1a79ba9f5b06
Erik Berger <berger.erik12@gmail.com>
2024-10-23 15:17:31 +0200

    Removed favorite products from products page

--- frontend/src/pages/products/ProductsPage.js
+++ frontend/src/pages/products/ProductsPage.js
@@ -16,7 +16,6 @@ import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
-import FavoriteProducts from "../profiles/FavoriteProducts";
 
 function ProductsPage({ message, filter = "" }) {
   const [products, setProducts] = useState({ results: [] });
@@ -33,7 +32,7 @@ function ProductsPage({ message, filter = "" }) {
         setProducts(data);
         setHasLoaded(true);
       } catch (err) {
-        // console.log(err);
+        // console.error(err);
       }
     };
 
@@ -50,7 +49,6 @@ function ProductsPage({ message, filter = "" }) {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <FavoriteProducts mobile />
         <i className={`fas fa-search ${styles.SearchIcon}`} />
         <Form
           className={styles.SearchBar}
@@ -92,9 +90,6 @@ function ProductsPage({ message, filter = "" }) {
             <Asset spinner />
           </Container>
         )}
-      </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <FavoriteProducts />
       </Col>
     </Row>
   );
