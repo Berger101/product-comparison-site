b4aeeddac58f428b04aa4da50200a567333bca77
Erik Berger <berger.erik12@gmail.com>
2024-10-24 13:11:49 +0200

    Updating favorite serializers with new fields and models

--- favorites/serializers.py
+++ favorites/serializers.py
@@ -1,19 +1,23 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Favorite
-from products.models import Product
 
 
 class FavoriteSerializer(serializers.ModelSerializer):
     """
     Serializer for the Favorite model
-    Create method handles the unique constraint on 'product' and 'user'
+    The create method handles the unique constraint on 'owner' and 'product'
     """
     owner = serializers.ReadOnlyField(source='owner.username')
-    product = serializers.PrimaryKeyRelatedField(
-        queryset=Product.objects.all(), write_only=True
-    )
-    product_name = serializers.ReadOnlyField(source='product.name')
 
     class Meta:
         model = Favorite
-        fields = ['id', 'owner', 'product', 'product_name', 'created_at']
+        fields = ['id', 'created_at', 'owner', 'product']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'This product is already favorited by you.'
+            })
