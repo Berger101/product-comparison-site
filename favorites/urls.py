c99ff84ff455df48bf5fdd5a0eccffcc9d7b159f
Erik Berger <berger.erik12@gmail.com>
2024-10-24 13:12:02 +0200

    Updating url path with new view names

--- favorites/urls.py
+++ favorites/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from favorites import views
 
 urlpatterns = [
-    path('favorites/', views.FavoriteListCreateView.as_view(),
+    path('favorites/', views.FavoriteList.as_view(),
          name='favorite-list-create'),
-    path('favorites/<int:pk>/', views.FavoriteDetailView.as_view(),
+    path('favorites/<int:pk>/', views.FavoriteDetail.as_view(),
          name='favorite-detail'),
 ]
