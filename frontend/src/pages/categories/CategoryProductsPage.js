5dfcb4da32a52923eabfa6d9043fe844edb3389e
Erik Berger <berger.erik12@gmail.com>
2024-10-25 14:06:01 +0200

    Removed infinite scroll from category page and introduced new styling

--- frontend/src/pages/categories/CategoryProductsPage.js
+++ frontend/src/pages/categories/CategoryProductsPage.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Product from "../products/Product";
 import styles from "../../styles/CategoryProductsPage.module.css";
-import { Container, Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Asset from "../../components/Asset";
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -31,37 +31,30 @@ function CategoryProductsPage() {
   const categoryTitle = category.charAt(0).toUpperCase() + category.slice(1);
 
   return (
-    <Container className={`${styles.CategoryProductsPage}`}>
-      <h2 className="text-center mb-4">{categoryTitle}</h2>
-      {hasLoaded ? (
-        categoryProducts.results.length ? (
-          <InfiniteScroll
-            className="row" // Bootstrap's row class for grid alignment
-            dataLength={categoryProducts.results.length}
-            loader={<Asset spinner />}
-            hasMore={!!categoryProducts.next}
-            next={() => fetchMoreData(categoryProducts, setCategoryProducts)}
-          >
-            {categoryProducts.results.map((product) => (
-              <Col
-                key={product.id}
-                xs={12} // Full width on mobile
-                md={6} // Two columns on medium and larger screens
-                className="mb-4"
-              >
-                <Product {...product} setProducts={setCategoryProducts} />
-              </Col>
-            ))}
-          </InfiniteScroll>
-        ) : (
-          <Asset
-            src={NoResults}
-            message={`No products found for the category "${categoryTitle}".`}
-          />
-        )
-      ) : (
-        <Asset spinner />
-      )}
+    <Container>
+      <Row className="justify-content-center">
+        <Col lg={8} className="py-2 p-0 p-lg-2 m-0">
+          <h2 className="text-center mb-4">{categoryTitle}</h2>
+          {hasLoaded ? (
+            categoryProducts.results.length ? (
+              categoryProducts.results.map((product) => (
+                <Row key={product.id} className="mb-4">
+                  <Col className="py-0 p-0 p-lg-2 m-0">
+                    <Product {...product} setProducts={setCategoryProducts} />
+                  </Col>
+                </Row>
+              ))
+            ) : (
+              <Asset
+                src={NoResults}
+                message={`No products found for the category "${categoryTitle}".`}
+              />
+            )
+          ) : (
+            <Asset spinner />
+          )}
+        </Col>
+      </Row>
     </Container>
   );
 }
