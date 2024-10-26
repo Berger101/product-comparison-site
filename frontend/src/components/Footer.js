43958ee5b956616de3b0ca2c5f838a724b8ac65b
Erik Berger <berger.erik12@gmail.com>
2024-10-26 12:57:11 +0200

    Updated footer links with link component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,35 +1,63 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
+  const currentUser = useCurrentUser();
+
   return (
     <footer className={styles.Footer}>
       <Container>
         <Row className="text-center py-3">
           <Col md={4}>
             <h5>About Us</h5>
-            <p>We provide the best product comparisons to help you make informed decisions.</p>
+            <p>
+              We provide the best product comparisons to help you make informed
+              decisions.
+            </p>
           </Col>
           <Col md={4}>
             <h5>Quick Links</h5>
             <ul className={styles.FooterLinks}>
-              <li><a href="/home">Home</a></li>
-              <li><a href="/products">Products</a></li>
-              <li><a href="/categories">Categories</a></li>
-              <li><a href="/contact">Contact Us</a></li>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/categories">Categories</Link>
+              </li>
+              {currentUser && (
+                <li>
+                  <Link to={`/profiles/${currentUser.profile_id}`}>
+                    Profile
+                  </Link>
+                </li>
+              )}
             </ul>
           </Col>
           <Col md={4}>
             <h5>Follow Us</h5>
             <div className={styles.SocialIcons}>
-              <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <i className="fab fa-facebook"></i>
               </a>
-              <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <i className="fab fa-twitter"></i>
               </a>
-              <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <i className="fab fa-instagram"></i>
               </a>
             </div>
@@ -38,7 +66,8 @@ const Footer = () => {
         <Row className="text-center">
           <Col>
             <p className={styles.Copyright}>
-              &copy; {new Date().getFullYear()} Product Comparison Site. All Rights Reserved.
+              &copy; {new Date().getFullYear()} Product Comparison Site. All
+              Rights Reserved.
             </p>
           </Col>
         </Row>
