380f046424e944cfb9c01c072043c9a0b389f260
Erik Berger <berger.erik12@gmail.com>
2024-10-28 20:27:23 +0100

    Updated axis defaults with /api endpoint

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -5,7 +5,16 @@ import axios from "axios";
 //axios.defaults.baseURL = "http://localhost:8000/api"; // Uncomment in local dev if needed
 // axios.defaults.baseURL = "/api";
 
-axios.defaults.baseURL = "/api";
+// axios.defaults.baseURL = "/api";
+
+axios.defaults.baseURL = process.env.NODE_ENV === 'production'
+  ? "/api"
+  : "http://localhost:8000/api";
+
+
+// axios.defaults.baseURL = process.env.NODE_ENV === 'development'
+//   ? "http://localhost:8000/api"
+//   : "/api";
 
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true; // Ensures cookies are sent with requests
