c3d1d18bccf318901c003d4c88592b407b1763c6
Erik Berger <berger.erik12@gmail.com>
2024-10-30 11:24:29 +0100

    Removed unused code

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -1,12 +1,9 @@
 import React from "react";
 import styles from "../styles/Avatar.module.css";
 
-// Define the fallback default image URL
-const defaultAvatar = "../assets/default_profile.jpg";
-
 const Avatar = ({ src, height = 45, text }) => {
   // Use the provided src, or fallback to the default image if src is not provided
-  const imageSrc = src || defaultAvatar;
+  const imageSrc = src;
 
   return (
     <span>
